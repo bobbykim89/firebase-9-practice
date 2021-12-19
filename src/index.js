c89317423e8321a3bda86c4716ef16a4af051fee
bobbykim89 <bobby.sihun.kim@gmail.com>
2021-12-18 23:55:09 -0700

    initialized the files by bundling webpack and adding firebase config

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAaezNLXZGvJ893pNEPbWaf1CUA8yYbtQU',
+  authDomain: 'fir-9-practice-fbf83.firebaseapp.com',
+  projectId: 'fir-9-practice-fbf83',
+  storageBucket: 'fir-9-practice-fbf83.appspot.com',
+  messagingSenderId: '269120716181',
+  appId: '1:269120716181:web:1f734dda23d4fa18aa0a9c',
+};
+
+initializeApp(firebaseConfig);
