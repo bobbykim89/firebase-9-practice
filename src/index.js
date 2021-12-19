184ef7b59d273071ee342dbce31348058db762c9
bobbykim89 <bobby.sihun.kim@gmail.com>
2021-12-19 03:23:05 -0700

    now it is getting realtime data

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import { initializeApp } from 'firebase/app';
 import {
   getFirestore,
   collection,
-  getDocs,
+  onSnapshot,
   addDoc,
   deleteDoc,
   doc,
@@ -26,18 +26,14 @@ const db = getFirestore();
 // collection ref
 const colRef = collection(db, 'books');
 
-// get collection data
-getDocs(colRef)
-  .then((snapshot) => {
-    let books = [];
-    snapshot.docs.forEach((doc) => {
-      books.push({ ...doc.data(), id: doc.id });
-    });
-    console.log(books);
-  })
-  .catch((err) => {
-    console.log(err.message);
+// realtime collection data
+onSnapshot(colRef, (snapshot) => {
+  let books = [];
+  snapshot.docs.forEach((doc) => {
+    books.push({ ...doc.data(), id: doc.id });
   });
+  console.log(books);
+});
 
 // adding documents
 const addBookForm = document.querySelector('.add');
